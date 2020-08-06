7869df70e57a79116f6f7151ebc80871664440b3
Amanjeev Sethi <aj@amanjeev.com>
2020-08-06 18:16:04 -0400

    adds: openapi

--- src/mesi_search/main.py
+++ src/mesi_search/main.py
@@ -1,14 +1,56 @@
 # -*- coding: utf-8 -*-
 """MESI application, main
 """
-from flask import Flask
+from flask import Flask, jsonify, request, Response
+from flasgger import swag_from, Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
+
+
+@swagger.definition('Pet')
+class Pet(object):
+    """
+    Pet Object
+    ---
+    properties:
+        name:
+            type: string
+    """
+    def __init__(self, name):
+        self.name = str(name)
+
+    def dump(self):
+        return dict(vars(self).items())
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return 'Welcome to MESI Search!'
+
+
+@app.route('/discover', methods=['POST'])
+@swag_from()
+def discover():
+    """
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    ---
+    description: Post a request body
+    requestBody:
+        content:
+            application/json:
+                schema:
+                    $ref: '#/definitions/Pet'
+        required: true
+    responses:
+        200:
+            description: The posted request body
+            content:
+                application/json:
+                    schema:
+                        $ref: '#/definitions/Pet'
+    """
+    return jsonify(request.json)
 
 
 if __name__ == '__main__':
