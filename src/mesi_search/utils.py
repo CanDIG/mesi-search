0849af5adbea3070b97ec9b4b8ef96c74bf58999
Amanjeev Sethi <aj@amanjeev.com>
2020-08-26 14:57:07 -0400

    adds (session): Rate limiting key func
    Rate limiting using Flask-Limiter by providing JWT `sub` claim to enforce per user limiting based on the passed JWT.

--- src/mesi_search/utils.py
+++ src/mesi_search/utils.py
@@ -7,6 +7,14 @@ from flask import request, Response, session
 logger = logging.getLogger(__name__)
 
 
+def flask_limiter_key():
+    limiter_key = None
+    sub = session.get("sub", None)
+    if sub:
+        limiter_key = sub
+    return limiter_key
+
+
 def get_jwt(req):
     """Checks Authorization: Bearer .... for JWT
     @param req: Flask `request` object
@@ -22,25 +30,34 @@ def get_jwt(req):
     return req_jwt
 
 
-def check_jwt(req):
-    """Check for JWT's validity
-
-    @param req: Flask `request` object
-    @return: boolean
-    """
-    is_request_authorized = False
+def decode_jwt(req):
+    decoded_jwt = {}
     req_jwt = get_jwt(req)
 
     if req_jwt != "":
         try:
-            _ = jwt.decode(req_jwt, verify=False)  # TODO: too bad, need to verify JWT
-            is_request_authorized = True
+            # TODO: need to verify JWT with pub key
+            # TODO: need to check for claims that Vault sends in JWT
+            decoded_jwt = jwt.decode(req_jwt, verify=False,
+                                     options={'require': ['iat', 'sub']})
         except jwt.InvalidIssuedAtError as iisa:
             logger.error("JWT has invalid Issued-At datetime ", str(iisa))
         except jwt.ExpiredSignatureError as es:
             logger.error("JWT has invalid or expired signature ", str(es))
         except jwt.DecodeError as d:
             logger.error("JWT could not be decoded for reasons ", str(d))
+    return decoded_jwt
+
+
+def is_jwt_valid(req):
+    """Check for JWT's validity
+
+    @param req: Flask `request` object
+    @return: boolean
+    """
+    is_request_authorized = False
+    if decode_jwt(req):
+        is_request_authorized = True
     return is_request_authorized
 
 
@@ -53,13 +70,15 @@ def authorize(f):
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not check_jwt(request):
+        if not is_jwt_valid(request):
             return Response("Missing valid authentication token. Please provide your "
                             "JWT in the Authorization header as Bearer token.", 401)
         else:
             logger.info("Authorized to make requests to the API")
+            token = decode_jwt(request)
             session["authorized"] = True
-            session["user"] = get_jwt(request)
+            session["user"] = token["sub"]
+            session["sub"] = token["sub"]
         return f(*args, **kwargs)
 
     return decorated_function
